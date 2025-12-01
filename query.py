baafdbc72aec6b84b12a2445e389cf2fb5fb431a
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2025-12-01 13:38:01 -0700

    refactored db connection to pandas, added url encoding for special char passwords

--- query.py
+++ query.py
@@ -1,39 +1,31 @@
-import pg8000
+from urllib import parse
+from sqlalchemy import create_engine, Engine
+import pandas as pd
 import getpass # temp
 
 # these can be globals defined in another file
 schema = 'kkubaska'
 table = 'jeffco_staging'
 
-def address_by_name(cursor: pg8000.Cursor, name: str):
+def address_by_name(engine: Engine, name: str):
     # should be updated, I think concating a null field makes the whole result null, so won't work for single owner homes
     query = "select " +\
         "ownnam || '|' || ownnam2 as owners, " +\
         "prpaddress || ', ' || prpctynam || ', ' || prpzip5 as address " +\
         f"from {schema}.{table} " +\
         "where ownnam ilike '%%' || %s || '%%' or ownnam2 ilike '%%' || %s || '%%';"
-    print(query)
-    cursor.execute(
+    return pd.read_sql(
         query,
-        [name, name])
-    return cursor.fetchall()
-
+        engine,
+        params=(name, name))
 
 def main():
     login = input('Login username: ')
-    secret = getpass.getpass()
-    credentials = {'user' : login,
-    'password': secret,
-    'database': 'csci403',
-    'host' : 'ada.mines.edu'}
-    db = pg8000.connect(**credentials)
-    cursor = db.cursor()
+    secret = parse.quote(getpass.getpass())
+    engine = create_engine(f'postgresql+psycopg2://{login}:{secret}@ada.mines.edu:5432/csci403')
 
-    results = address_by_name(cursor, 'mcdonald')
-    for owners, address in results:
-        print(owners, address, sep="    ")
+    results = address_by_name(engine, 'mcdonald')
+    print(results)
     
-
-
 if __name__ == "__main__":
     main()
